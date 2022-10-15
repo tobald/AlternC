3d747b40b1c1fff501757e7dfeea42d9bb641c8b
quenenni <la.jolie@paquerette.org>
2022-10-15 21:24:26 +0200

    templates/roundcube/plugins/managesieve: updated managesieve config template to v1.4.13 (Debian buster Roundcube version atm)

--- roundcube/templates/roundcube/plugins/managesieve/config.inc.php
+++ roundcube/templates/roundcube/plugins/managesieve/config.inc.php
@@ -9,7 +9,8 @@
 //                                                                     //
 /***********************************************************************/
 
-// managesieve server port
+// managesieve server port. When empty the port will be determined automatically
+// using getservbyname() function, with 4190 as a fallback.
 $rcmail_config['managesieve_port'] = 4190;
 
 // managesieve server address, default is localhost.
@@ -33,11 +34,23 @@ $rcmail_config['managesieve_auth_cid'] = null;
 $rcmail_config['managesieve_auth_pw'] = null;
 
 // use or not TLS for managesieve server connection
-// it's because I've problems with TLS and dovecot's managesieve plugin
-// and it's not needed on localhost
+// Note: tls:// prefix in managesieve_host is also supported
 $rcmail_config['managesieve_usetls'] = false;
 
-// default contents of filters script (eg. default spam filter)
+// Connection scket context options
+// See http://php.net/manual/en/context.ssl.php
+// The example below enables server certificate validation
+//$rcmail_config['managesieve_conn_options'] = array(
+//  'ssl'         => array(
+//     'verify_peer'  => true,
+//     'verify_depth' => 3,
+//     'cafile'       => '/etc/openssl/certs/ca.crt',
+//   ),
+// );
+// Note: These can be also specified as an array of options indexed by hostname
+$rcmail_config['managesieve_conn_options'] = null;
+
+// A file with default script content (eg. spam filter)
 $rcmail_config['managesieve_default'] = '/etc/dovecot/sieve/global';
 
 // The name of the script which will be used when there's no user script
@@ -72,5 +85,56 @@ $rcmail_config['managesieve_filename_extension'] = '.sieve';
 // List of reserved script names (without extension).
 // Scripts listed here will be not presented to the user.
 $rcmail_config['managesieve_filename_exceptions'] = array();
+
+// List of domains limiting destination emails in redirect action
+// If not empty, user will need to select domain from a list
+$rcmail_config['managesieve_domains'] = array();
+
+// Default list of entries in header selector
+$rcmail_config['managesieve_default_headers'] = array('Subject', 'From', 'To');
+
+// Enables separate management interface for vacation responses (out-of-office)
+// 0 - no separate section (default),
+// 1 - add Vacation section,
+// 2 - add Vacation section, but hide Filters section
+$rcmail_config['managesieve_vacation'] = 0;
+
+// Enables separate management interface for setting forwards (redirect to and copy to)
+// 0 - no separate section (default),
+// 1 - add Forward section,
+// 2 - add Forward section, but hide Filters section
+$rcmail_config['managesieve_forward'] = 0;
+
+// Default vacation interval (in days).
+// Note: If server supports vacation-seconds extension it is possible
+// to define interval in seconds here (as a string), e.g. "3600s".
+$rcmail_config['managesieve_vacation_interval'] = 1;
+
+// Some servers require vacation :addresses to be filled with all
+// user addresses (aliases). This option enables automatic filling
+// of these on initial vacation form creation.
+$rcmail_config['managesieve_vacation_addresses_init'] = false;
+
+// Sometimes you want to always reply with mail email address
+// This option enables automatic filling of :from field on initial vacation form creation.
+$rcmail_config['managesieve_vacation_from_init'] = false;
+
+// Supported methods of notify extension. Default: 'mailto'
+$rcmail_config['managesieve_notify_methods'] = array('mailto');
+
+// Enables scripts RAW editor feature
+$rcmail_config['managesieve_raw_editor'] = true;
+
+// Disabled actions
+// Prevent user from performing specific actions:
+// list_sets, enable_disable_set, delete_set, new_set, download_set, new_rule, delete_rule
+// Note: disabling list_sets removes the Filter sets widget from the UI and means
+//       the set defined in managesieve_script_name will always be used (and activated)
+$rcmail_config['managesieve_disabled_actions'] = array();
+
+// List of hosts that support managesieve.
+// Activate managesieve for selected hosts only. If this is not set all hosts are allowed.
+// Example: $rcmail_config['managesieve_allowed_hosts'] = array('host1.mydomain.com','host2.mydomain.com');
+$rcmail_config['managesieve_allowed_hosts'] = null;
 
 ?>
